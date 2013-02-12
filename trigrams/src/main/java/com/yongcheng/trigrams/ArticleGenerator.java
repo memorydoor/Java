62bef1be6c080ee46a159cebfffc1a1133dbb6e9
cochen <yong.cheng@emerson.com>
2013-02-12 17:32:00 -0500

    Implemented ArticleGernerator

--- trigrams/src/main/java/com/yongcheng/trigrams/ArticleGenerator.java
+++ trigrams/src/main/java/com/yongcheng/trigrams/ArticleGenerator.java
@@ -1,5 +1,29 @@
 package com.yongcheng.trigrams;
 
 public class ArticleGenerator {
+	private IPairWordsProcessor pairWordsProcessor;
 
+	private final StringBuilder article = new StringBuilder();
+
+	public void generate() {
+		this.article.setLength(0);
+
+		String startingPoint = this.pairWordsProcessor.getRandomLeft();
+
+		this.article.append(startingPoint);
+
+		String left = startingPoint;
+		String right = null;
+
+		while ((right = this.pairWordsProcessor.getRight(left)) != null) {
+			this.article.append(" ");
+			this.article.append(right);
+
+			left = left.substring(left.indexOf(" ") + 1) + " " + right;
+		}
+	}
+
+	public String getArticle() {
+		return this.article.toString();
+	}
 }
